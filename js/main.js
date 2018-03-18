aa59675da8f7e1cd580e2d8faf25d8ea9b16641e
8dragan8 <35200825+8dragan8@users.noreply.github.com>
2018-03-18 18:42:13 +0100

    Ubacen metod za pravljenje tabova

--- js/main.js
+++ js/main.js
@@ -1,78 +1,107 @@
-// $(document).ready(function(){
-//     $(".red-harmonika").click(function(e){
-//         console.log('faktura');
-//         $(this).toggleClass('active');
-//         $(this).next().collapse('toggle');
-//     });
+$(document).ready(function(){
+    $(".red-harmonika").click(function(e){
+
+        var ovo = this.getAttribute('brojReda');
+        var skriveniRed = '#' + ovo + '-skriveni-red';
+        var idNaloga = this.getAttribute('idNaloga');
+        var idfakture = this.getAttribute('idfakture');
+
+
+
+        console.log('faktura');
+        $(this).toggleClass('active');
+        $(this).next().collapse('toggle');
+
+        if (!$(skriveniRed).attr("ucitano")) {
+            
+            tabs(ovo, idNaloga, idfakture, skriveniRed);
+        }
+
+    });
+
+    // $('.nav-tabs > li').click(function(){
+
+    //     var ckliknutiTab = $(this).find('a').attr('href');
+    //     $(this.parentNode).next('.tab-content').find('.active').toggleClass('in active').end();
+    //     $(this.parentNode).next('.tab-content').find(ckliknutiTab).addclass('in active').end();
+    // })
    
-//     $(".phpBrNaloga-dugme").click(function(e){
-//     var red = this.parentNode.parentNode;
-//     console.log('dugme');
-//         $(red).toggleClass('active');
-//         $(red).next().collapse('toggle');
-//     });
+    
+});
+
+
+
+// function faktura (elem) {
+//         var ovo = elem.getAttribute('brojReda');
+//         var aktivniRed = '#' + ovo + '-red';
+//         var skriveniRed = '#' + ovo + '-skriveni-red';
+//         var idfakture = elem.value;
+//         $(aktivniRed).next().collapse('toggle');
+//         if (!$(elem).attr("ucitano")) {
+            
+//             ajaxUpit('prikaz fakture.php', idfakture, skriveniRed);
+//             $(elem).attr("ucitano", "true");
+//         }
+//     };
+
+
+
+// function nalog (elem) {
+//         var ovo = elem.getAttribute('brojReda');
+//         var aktivniRed = '#' + ovo + '-red';
+//         var skriveniRed = '#' + ovo + '-skriveni-red';
+//         var idNaloga = elem.value;
+//         $(aktivniRed).next().collapse('toggle');
+//         if (!$(elem).attr("ucitano")) {
+
+//             ajaxUpit('prikaz naloga.php', idNaloga, skriveniRed);
+//             $(elem).attr("ucitano", "true");
+//         }
+//     };
+
+
+function ajaxUpit (php, parametar, meta) {
+
+        $.ajax({
+            method: "POST",
+            url: php,
+            data: { nalog: parametar, faktura: parametar }
+          }).done(
+            function(result) {
+                $(meta).append(result);
+          });
+
+    };
+
+
+function tabs (ovo, idNaloga, idfakture, skriveniRed) {
+
+    var tabovi = "<ul class='nav nav-tabs'>";
+    var menu1 = "#menu1-" + ovo;
+    var menu2 = "#menu2-" + ovo;
+
+
+    tabovi += "<li class='active'> <a data-toggle='tab' href='#menu1-" + ovo +"'>Nalog</a></li>";
+    tabovi += "<li><a data-toggle='tab' href='#menu2-" + ovo +"'>Faktura</a></li>";
+    tabovi += "</ul>";
+    
+    tabovi += "<div class='tab-content'>";
+    tabovi += "<div id='menu1-" + ovo +"' class='tab-pane active'>";
+    // tabovi += menu1;
+    tabovi += "</div>";
+    tabovi += "<div id='menu2-" + ovo +"' class='tab-pane'>";
+    // tabovi += menu2;
+    tabovi += "</div>";
+    tabovi += "</div>";
+    
+    $(skriveniRed).append(tabovi);
+    $(skriveniRed).attr("ucitano", "true");
+    
+    ajaxUpit('prikaz naloga.php', idNaloga, menu1);
+    ajaxUpit('prikaz fakture.php', idfakture, menu2);
 
     
-// });
-
-// $(function () {
-
-//     $('.form-faktura').on('submit', function (e) {
-//         // $('#skriveno').next().collapse('toggle');
-
-//       e.preventDefault();
-//       var parametar = 'faktura=' + this.childNodes[1].childNodes[1].value;
-
-//       $.ajax({
-//         type: 'post',
-//         url: 'prikaz fakture.php',
-//         data: parametar,
-//     }).done(
-//         function (result) {
-//           $('#txtHint-0').append(result);
-//         }
-
-//     )
-
-//     });
-
-
-//   });
-
-function faktura (elem) {
-        var ovo = elem.getAttribute('brojReda');
-        var aktivniRed = '#' + ovo + '-red';
-        var skriveniRed = '#' + ovo + '-skriveni-red';
-        var parametar = elem.value;
-        $(aktivniRed).next().collapse('toggle');
-        if (!$(elem).attr("ucitano")) {
-            
-            var posting = $.post("prikaz fakture.php", {faktura: parametar});
-            posting.done(
-                function(result) {
-                    $(skriveniRed).empty().append(result);
-                    $(elem).attr("ucitano", "true");
-                }
-            );
-        }
-    };
 
 
 
-function nalog (elem) {
-        var ovo = elem.getAttribute('brojReda');
-        var aktivniRed = '#' + ovo + '-red';
-        var skriveniRed = '#' + ovo + '-skriveni-red';
-        var parametar = elem.value;
-        $(aktivniRed).next().collapse('toggle');
-        if (!$(elem).attr("ucitano")) {
-
-            var posting = $.post("prikaz naloga.php", {nalog: parametar});
-            posting.done(
-                function(result) {
-                    $(skriveniRed).empty().append(result);
-                    $(elem).attr("ucitano", "true");
-                }
-            );
-        }
-    };
+};
